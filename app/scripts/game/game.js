fef6b8eb956860799d5b80d2b7cb11035428ba6f
MarianYakubets <marian.yakubets@gmail.com>
2015-11-15 22:18:58 +0200

    h

--- app/scripts/game/game.js
+++ app/scripts/game/game.js
@@ -1,12 +1,8 @@
 angular.module('Game', [])
 .service('GameManager', function() {
-  // Создать новую игру
   this.newGame = function() {};
-  // Обработка хода
   this.move = function() {};
-  // Обновление очков
   this.updateScore = function(newScore) {};
-  // Остались ли ещё ходы?
   this.movesAvailable = function() {
     return GridService.anyCellsAvailable() ||
       GridService.tileMatchesAvailable();
